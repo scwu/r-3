871125adb0b34598b0d1f446062373c0520e6b3e
Clara Wu <s.clara.wu@gmail.com>
2013-01-19 15:42:42 -0500

    added ajax functionality

--- r3/static/js/script.js
+++ r3/static/js/script.js
@@ -7,5 +7,34 @@ $(document).ready(function() {
 		var return_date = $('inputField2').val();
 		var own_car = $('input#car').is(':checked');
 		var can_drive = $('input#drive').is(':checked');
-	}
+	
+		$.getJSON($SCRIPT_ROOT + '/_find', {
+            ori : origin,
+            dest : destination,
+            leave : leave_date,
+            return_d : return_date,
+            own : own_car,
+            can : can_drive
+         }, function(data) {
+              $.each(data, function(k, v) {
+                  try {
+                    var img = v[0].img;
+                    var name = v[0].name;
+                    var url = v[0].url;
+                    $('#searchresults').html('<p id = "' + k + '"><a href="' + url + '">' + '<img src="' + img + '&s=50"></img>' 
+                           + '<span>' + name + '</span></a>');
+                    var content = $('div#searchresults').height();
+                    $('#ajaxresults').height(content);
+                    console.log(content);
+                  }
+                  catch (err) {
+                    $('#ajaxresults').height(25);
+                    $('#searchresults').html('<p>Sorry, no results found.</p>');
+                  }
+              });
+         });
+
+	});
+
+	
 };
--- r3/views.py
+++ r3/views.py
@@ -5,3 +5,17 @@ from django.shortcuts import render_to_response
 
 def hello(request):
 	return render_to_response('index.html')
+
+def find(request):
+	if not request.POST:
+		return render_to_response('index.html')
+	origin = request.POST['ori']
+	destination = request.POST['dest']
+	leave_date = request.POST['leave']
+	return_date = request.POST['return_d']
+	own_car = request.POST['own']
+	can_drive = request.POST['can_drive']
+	#ALGO HERE YO
+	#MAKE THAT SHIT JSON!!!!
+	#send it back to js
+	
