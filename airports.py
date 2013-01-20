950df3820022e88e91301161825bae07273b1edc
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2013-01-19 22:14:52 -0500

    Added airports file

---
+++ airports.py
@@ -0,0 +1,13 @@
+
+northeast = set(["Philadelphia, Pennsylvania (PHL)", "Boston, Massachusetts (BOS)", "Baltimore, Maryland (BWI)", "Hartford, Connecticut (BDL)", "New York, New York (JFK)", "New York, New York (LGA)", "Newark, New Jersey (EWR)", "Washington, D.C. (IAD)", "Washington, D.C. (DCA)"])
+texas = set(["San Antonio, Texas (SAT)", "Austin, Texas (AUS)", "Dallas, Texas (DFW)", "Houston, Texas (IAH)", "Houston, Texas (HOU)"]
+northwest = set(["Portland, Oregon (PDX)", "Seattle, Washington (SEA)"])
+rockies = set(["Salt Lake City, Utah (SLC)", "Denver, Colorado (DEN)"])
+california = set(["Los Angeles, California (LAX)", "San Francisco, California (SFO)", "Oakland, California (OAK)", "Sacremento, California (SMF)", "San Diego, California (SAN)", "San Jose, California (SJC)"])
+midwest = set(["St Louis, Missouri (STL)", "Louisville, Kentucky (SDF)", "Indianapolis, Indiana (IND)"])
+southwest = set(["Las Vegas, Nevada (LAS)", "Phoenix, Arizona (PHX)"])
+florida = set(["Fort Lauderdale, Florida (FLL)", "Fort Meyers, Florida (RSW)", "Miami, Florida (MIA)", "Orlando, Florida (MCO)", "Tampa, Florida (TPA)"])
+southeast = set(["Atlanta, Georgia (ATL)", "Memphis, Tennessee (MEM)", "Nashville, Tennessee (BNA)", "New Orleans, Louisiana (MSY)"])
+greatLakes = set(["Minneapolis, Minnesota (MSP)", "Chicago, Illinois (MDW)", "Chicago, Illinois (ORD)", "Detroit, Michigan (DTW)"])
+ohio = set(["Cleveland, Ohio (CLE)", "Cincinnati, Ohio (CVG)", "Columbus, Ohio (CMH)", "Pittsburgh, Pennsylvania (PIT)"])
+northCarolina = set(["Charlotte, North Carolina (CLT)", "Raleigh, North Carolina (RDU)"])
