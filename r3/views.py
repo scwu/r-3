bf9c7ea3c0f69d505dfee4383946975dc217fdc4
Clara Wu <s.clara.wu@gmail.com>
2013-01-19 23:26:49 -0500

    sorry, im dumb

--- r3/views.py
+++ r3/views.py
@@ -38,8 +38,8 @@ def find(request):
 
 def getDirectionsDriving {
 	api_driving = 'http://maps.googleapis.com/maps/api/directions/json?'
-	origin_api = 'origin=' + urllib2.quote("32 Endicott Drive, Amherst New York")
-	destination_api = 'destination=' + urllib2.quote("206 Brownshill Road, Pittsburgh Pennsylvania")
+	origin_api = 'origin=' + urllib2.quote(origin)
+	destination_api = 'destination=' + urllib2.quote(destination)
 	final_drive = api_driving + origin_api + '&' + destination_api + '&sensor=false'
 	response = urllib2.urlopen(final_drive)
 	directions_drive = simplejson.loads(response.read())
