582e002b2f470d61335e85dedc5743e261ee0de7
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2013-01-20 00:57:17 -0500

    Created the basic Google Maps Direction API file that will return the distance and duration of a trip. will need to handle different time formats in the future

---
+++ directions.py
@@ -0,0 +1,13 @@
+import urllib2
+import simplejson as json
+
+def getDirections(origin, destination, time):
+	api_str = 'http://maps.googleapis.com/maps/api/directions/json?'
+	origin_str = 'origin=' + urllib2.quote(origin)
+	destination_str = 'destination=' + urllib2.quote(destination)
+	time_str = 'departure_time=' + time#will have to convert to epoch 
+	final_str = api_str + origin_str + '&' + destination_str + '&sensor=false' + '&' + time_str + '&mode=transit'
+	response = urllib2.urlopen(final_str)
+	directions = json.loads(response.read())
+	distance = int(directions['routes'][0]['legs'][0]['distance']['text'][:2])
+	duration = float(directions['routes'][0]['legs'][0]['duration']['value'])/(60)
