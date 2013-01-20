4294a922fe9adac7bda5fefaa35c20d50d3e82f9
Clara Wu <s.clara.wu@gmail.com>
2013-01-20 05:04:24 -0500

    updated models, do python manage.py syncdb

--- r3/models.py
+++ r3/models.py
@@ -4,9 +4,11 @@ class Routes(models.Model):
 	origin = models.CharField(max_length=500)
 	destination = models.CharField(max_length=500)
 	origin_address = models.CharField(max_length=500)
+	origin_time = models.CharField(max_length=500)
 	destination_address = models.CharField(max_length=500)
+	destination_time = models.CharField(max_length=500)
 	brand = models.CharField(max_length=500)
 	types = models.CharField(max_length=100)
 	cost = models.CharField(max_length=100)
+	current_time = models.CharField(max_length=100)
 	time = models.CharField(max_length=100)
-	
