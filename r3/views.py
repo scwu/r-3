50d50ee7bbd4ef5818dc3acbc79f11a419d014c0
Clara Wu <s.clara.wu@gmail.com>
2013-01-19 23:24:39 -0500

    added google maps api

--- r3/views.py
+++ r3/views.py
@@ -4,7 +4,17 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext, Template
 from models import Routes
-import json
+from BeautifulSoup import BeautifulSoup
+import simplejson as json
+import urllib
+import urllib2
+
+origin = ''
+destination = ''
+leave_date = ''
+return_date = ''
+own_car = ''
+can_drive = ''
 
 def hello(request):
 	return render_to_response('index.html')
@@ -23,4 +33,15 @@ def find(request):
 	#send it back to js
 	message = origin + destination + leave_date + return_date + own_car
 	return HttpResponse(json.dumps({"MESSAGE":message}), mimetype="application/json")
+
+#python functions
+
+def getDirectionsDriving {
+	api_driving = 'http://maps.googleapis.com/maps/api/directions/json?'
+	origin_api = 'origin=' + urllib2.quote("32 Endicott Drive, Amherst New York")
+	destination_api = 'destination=' + urllib2.quote("206 Brownshill Road, Pittsburgh Pennsylvania")
+	final_drive = api_driving + origin_api + '&' + destination_api + '&sensor=false'
+	response = urllib2.urlopen(final_drive)
+	directions_drive = simplejson.loads(response.read())
 	
+}
